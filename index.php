f86e0554a1a0f2c76b08b96e967cfee543090da2
孟晉 <b82051420032gmail.com>
2017-09-30 14:20:38 +0800

    增加首頁

---
+++ index.php
@@ -0,0 +1,37 @@
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>	
+	<title>後台管理系統</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="./jquerymobile/jquery.mobile-1.4.5.min.css" />
+	<script src="./jquerymobile/jquery-2.1.0.min.js"></script>
+	<script src="./jquerymobile/jquery.mobile-1.4.5.min.js"></script>
+	<meta charset="UTF-8">
+
+
+
+</head>
+<body>
+<div data-role="page" id="home" >
+	<div data-role="header">
+		<h3>首頁</h3>
+	</div>
+	<div role="main" class="ui-content">
+		<div data-role="fieldcontain">
+
+    	<div class="ui-grid-a">
+    	<div class="ui-block-a"><a href="#" data-role="button" data-theme="b" id="go_regi">註冊</a></div>
+    	<div class="ui-block-b"><a href="#" data-role="button" data-theme="b" id="login">登入</a></div>
+    	
+    	</div> 	
+	
+	<div data-role="footer" data-position="fixed">
+		<h3>08號黃東俞</h3>
+	</div>
+</div>
+		
+</body>
+</html>
